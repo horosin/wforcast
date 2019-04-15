f2bb80f20f9922933b868fae6171378cd1802829
Karol Horosin <karol.horosin@gmail.com>
2019-04-15 21:39:28 +0100

    Update e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to wforecast!');
+    expect(page.getTitleText()).toEqual('Weather forecast for the next 5 days');
   });
 
   afterEach(async () => {
